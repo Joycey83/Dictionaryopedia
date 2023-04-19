fe60c61761b8214f324d9dc85118d26def9b3dcb
Joycey <Jcheung8385@gmail.com>
2023-04-19 11:13:53 +0100

    imported useState so we make changes on the input form

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,10 +1,22 @@
+import { useState } from "react";
 import "./Dictionary.css";
+
 const Dictionary = () => {
+  const [keyword, setKeyword] = useState("");
+
+  const inputHandle = (event) => {
+    setKeyword(event.target.value);
+  };
+
+  const search = (event) => {
+    event.preventDefault();
+    alert(`Searching... ${keyword}`);
+  };
   return (
     <>
       <div className="dictionary">
-        <form>
-          <input type="search" autoFocus={true} />
+        <form onSubmit={search}>
+          <input type="search" autoFocus={true} onChange={inputHandle} />
         </form>
       </div>
     </>
