ccbe7ef6161f931559e5797374d5445cd9834993
Joycey <Jcheung8385@gmail.com>
2023-04-20 11:03:22 +0100

    created a results compoenent so we can render the definitions of the words on the screen. imported the component into dictionary.js

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import axios from "axios";
+import Results from "./Results";
 import "./Dictionary.css";
 
 const Dictionary = () => {
@@ -26,6 +27,7 @@ const Dictionary = () => {
         <form onSubmit={search}>
           <input type="search" autoFocus={true} onChange={inputHandle} />
         </form>
+        <Results />
       </div>
     </>
   );
---
+++ src/Results.js
@@ -0,0 +1,10 @@
+const Results = () => {
+  return (
+    <>
+      <div className="results">
+        <h3>Hi Results</h3>
+      </div>
+    </>
+  );
+};
+export default Results;
