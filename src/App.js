1535fd3985d99e352fcdd2a290f36bffdaff0d09
Joycey <Jcheung8385@gmail.com>
2023-04-20 13:48:14 +0100

    I thought my CSS was not working but afterwards I realised I wasn't looking at my local server version but the netlify which the code hasn't been pushed to git yet

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <div className="container">
         <header className="App-header">
-          <h1 className="mt-5">Hello Dictionary App</h1>
+          <h1 className="mt-5">Dictionary App</h1>
         </header>
         <main>
           <Dictionary />
