a5fd6c6b1abbc3247615b9d8bca12e2b6707f83a
Joycey <Jcheung8385@gmail.com>
2023-04-24 17:33:33 +0100

    loop through all the images that is available for that word and rendered it on the screen

--- src/Photos.js
+++ src/Photos.js
@@ -1,4 +1,29 @@
 const Photos = (props) => {
-  return <div>Photos</div>;
+  console.log(props.photos);
+  if (props.photos) {
+    return (
+      <div className="Photos">
+        <div className="row">
+          {props.photos.map(function (photo, index) {
+            if (index < 9) {
+              return (
+                <div className="col-4" key={index}>
+                  <a href={photo.src.original} target="_blank">
+                    <img
+                      src={photo.src.landscape}
+                      alt={photo.alt}
+                      className="img-fluid"
+                    />
+                  </a>{" "}
+                </div>
+              );
+            }
+          })}{" "}
+        </div>
+      </div>
+    );
+  } else {
+    return null;
+  }
 };
 export default Photos;
