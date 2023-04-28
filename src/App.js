cf61783629ded822d1c361e583a9d6a08a281676
Joycey <Jcheung8385@gmail.com>
2023-04-28 14:10:08 +0100

    transferred my App.jsx file from my other repo into the app.js

--- src/App.js
+++ src/App.js
@@ -2,15 +2,11 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <div className="container">
-        <header className="App-header">
-          <h1 className="mt-5">Dictionary App</h1>
-        </header>
-        <main></main>
-        <footer className="app-footer">coded by Joyce Cheung</footer>
+    <>
+      <div className="app--container">
+        <h1>Dictionaryopedia</h1>
       </div>
-    </div>
+    </>
   );
 }
 
