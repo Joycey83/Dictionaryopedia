767835dd5bd027fe9ac86cc150955ad819af35d0
Joycey <Jcheung8385@gmail.com>
2023-04-28 14:14:59 +0100

    copy and pasted all my dictionary code from my other component into the new one

---
+++ src/components/Dictionaryopedia.js
@@ -0,0 +1,52 @@
+const Dictionaryopedia = () => {
+  const [keyword, setKeyword] = useState("");
+  const [definition, setDefinition] = useState(null);
+  const [photos, setPhotos] = useState(null);
+
+  const inputKeywordHandle = (event) => {
+    setKeyword(event.target.value);
+  };
+
+  const handleDictionaryResponse = (response) => {
+    // console.log(response.data[0]);
+    setDefinition(response.data[0]);
+  };
+
+  const showPhotos = (response) => {
+    console.log(response.data);
+    setPhotos(response.data.photos);
+  };
+
+  const searchWord = (event) => {
+    event.preventDefault();
+
+    // Free dictionary API
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(apiUrl).then(handleDictionaryResponse);
+
+    // Shecodes Image API
+    let photoApiKey = "a3a5bbeff1t6546488o90285fa5b0853";
+    let photoApiUrl = `https://api.shecodes.io/images/v1/search?query=${keyword}&key=${photoApiKey}`;
+    axios.get(photoApiUrl).then(showPhotos);
+  };
+  return (
+    <>
+      <div className={styles["dictionary--container"]}>
+        <section>
+          <form onSubmit={searchWord}>
+            <input
+              type="search"
+              placeholder="Search for a word"
+              autoFocus={true}
+              className="form-control search-input mt-3"
+              onChange={inputKeywordHandle}
+            />
+          </form>
+        </section>
+        <SearchResult definition={definition} />
+        <Photos photos={photos} />
+      </div>
+    </>
+  );
+};
+export default Dictionaryopedia;
