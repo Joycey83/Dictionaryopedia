622553d90988ab71373a3fad362735c82aa14c69
Joycey <Jcheung8385@gmail.com>
2023-04-29 13:00:15 +0100

    added dictionary logo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import "./App.css";
+import styles from "./App.module.css";
+import dictionaryLogo from "./images/dictionary-book.png";
 import Dictionaryopedia from "./components/Dictionaryopedia";
 
 function App() {
   return (
     <>
-      <div className="app--container">
+      <div className={styles["app--container"]}>
+        <div>
+          <img
+            src={dictionaryLogo}
+            alt="Dictionary A-Z"
+            className={styles["dictionary--logo"]}
+          />
+        </div>
         <Dictionaryopedia />
       </div>
     </>
