921a1bab041d418d9b8b534b5bdd77f02257d8f1
Joycey <Jcheung8385@gmail.com>
2023-04-30 09:49:09 +0100

    added the github fontawesome Icon to test if its working

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,11 @@ import styles from "../components/Footer.module.css";
 const Footer = () => {
   return (
     <>
-      <ul className={styles["footer--container"]}>Footer</ul>
+      <ul className={styles["footer--container"]}>
+        <li>
+          <i class="fa-brands fa-github"></i>
+        </li>
+      </ul>
     </>
   );
 };
