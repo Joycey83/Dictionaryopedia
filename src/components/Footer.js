9de33b6479da62dac8aded72d18ddc3a136cfd42
Joycey <Jcheung8385@gmail.com>
2023-04-30 10:16:27 +0100

    added a footer element in the footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,11 +2,9 @@ import styles from "../components/Footer.module.css";
 const Footer = () => {
   return (
     <>
-      <ul className={styles["footer--container"]}>
-        <li>
-          <i class="fa-brands fa-github"></i>
-        </li>
-      </ul>
+      <footer className={styles["footer--container"]}>
+        <ul className={styles["footer--icon"]}></ul>
+      </footer>
     </>
   );
 };
