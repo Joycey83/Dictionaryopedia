0d3371d68c1e08eb5941feed1f68194763b4d680
Joycey <Jcheung8385@gmail.com>
2023-04-30 19:53:36 +0100

    fixing some bugs on the photo component

--- src/components/Photos.js
+++ src/components/Photos.js
@@ -3,31 +3,25 @@ const Photo = (props) => {
   console.log(props.photos);
   if (props.photos) {
     return (
-      <>
-        <div className="Photos">
-          <div className="row">
-            {props.photos.map(function (photo, index) {
-              if (index < 9) {
-                return (
-                  <div className="col-lg-4 col-md-4" key={index}>
-                    <a
-                      href={photo.src.original}
-                      target="_blank"
-                      rel="noreferrer"
-                    >
-                      <img
-                        src={photo.src.landscape}
-                        alt={photo.alt}
-                        className={`img-fluid ${styles["word--img"]}`}
-                      />
-                    </a>{" "}
-                  </div>
-                );
-              }
-            })}{" "}
-          </div>
+      <div className="Photos">
+        <div className="row">
+          {props.photos.map(function (photo, index) {
+            if (index < 9) {
+              return (
+                <div className="col-lg-4 col-md-4" key={index}>
+                  <a href={photo.src.original} target="_blank" rel="noreferrer">
+                    <img
+                      src={photo.src.landscape}
+                      alt={photo.alt}
+                      className={`img-fluid ${styles["word--img"]}`}
+                    />
+                  </a>{" "}
+                </div>
+              );
+            }
+          })}{" "}
         </div>
-      </>
+      </div>
     );
   } else {
     return null;
