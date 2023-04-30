46b03b5db8961a558ffecae78603c539ca5551a2
Joycey <Jcheung8385@gmail.com>
2023-04-30 10:02:15 +0100

    added my instagram and githib page link and fontawesome Icon to my data.js file

--- src/data.js
+++ src/data.js
@@ -1,1 +1,17 @@
-export const socialLinks = [{ id: 1, href: "", icon: "" }];
+export const socialLinks = [
+  {
+    id: 1,
+    href: "https://github.com/Joycey83/Dictionaryopedia",
+    icon: "fa-brands fa-github",
+  },
+  {
+    id: 2,
+    href: "https://github.com/Joycey83/Dictionaryopedia",
+    icon: "",
+  },
+  {
+    id: 3,
+    href: "https://www.instagram.com/i_can_code_too/",
+    icon: "fa-brands fa-instagram",
+  },
+];
