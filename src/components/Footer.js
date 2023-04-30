b094315558326f41b68bd84b00f3b51c6bc5a132
Joycey <Jcheung8385@gmail.com>
2023-04-30 10:39:28 +0100

    destructured the props inside the social links function

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,14 @@
 import styles from "../components/Footer.module.css";
 import SocialLinks from "./SocialLinks";
 import { socialLinks } from "../data";
+
 const Footer = () => {
   return (
     <>
       <footer className={styles["footer--container"]}>
         <ul className={styles["footer--icon"]}>
           {socialLinks.map((link) => {
-            return <h1>item</h1>;
+            return <SocialLinks key={link.id} />;
           })}
         </ul>
       </footer>
--- src/components/SocialLinks.js
+++ src/components/SocialLinks.js
@@ -1,4 +1,12 @@
-const SocialLinks = () => {
-  return <div>SocialLinks</div>;
+const SocialLinks = ({ href, icon, itemClass }) => {
+  return (
+    <>
+      <li>
+        <a href={href} target="_blank" rel="noreferrer" className={itemClass}>
+          <i className={icon}></i>
+        </a>
+      </li>
+    </>
+  );
 };
 export default SocialLinks;
